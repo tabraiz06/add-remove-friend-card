d52f75397696b334185a0a9ce65db70b3b007c43
tabraiz06 <138916881+tabraiz06@users.noreply.github.com>
2023-09-11 10:42:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let istatus= document.querySelector("h5")
+
+let btn=document.querySelector("#add button")
+
+let check=0;
+
+btn.addEventListener("click", function(){
+   if(check===0){
+    istatus.innerHTML = "Friends"
+    istatus.style.color = "green"
+    btn.innerHTML="Remove Friend"
+    check=1
+    
+   }
+   else{
+    istatus.innerHTML = "Stranger"
+    istatus.style.color = "red"
+    btn.innerHTML="Add Friend"
+    
+    check=0
+    
+   }
+})
+
